a5e40f67ee9911d6d1ccb36eedcb9d4d9fbe5bcb
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-13 01:38:54 -0700

    Creat a button function

---
+++ app.js
@@ -0,0 +1,78 @@
+
+
+function clearScreen (){
+const input = document.querySelector(".input");
+input.innerHTML = "<p></p>"
+}
+function del (){
+const input =  document.querySelector(".input") ;
+input.innerHTML ="<p></p>"
+}
+function modulus (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>%</p>"
+}
+function divid (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>/</p>"
+}
+function seven (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>7</p>"
+}
+function eight (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>8</p>"
+}
+function nine (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>9</p>"
+}
+function multi (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>*</p>"
+}
+function four (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>4</p>"
+}
+function five (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>5</p>"
+}
+function six (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>6</p>"
+}
+function sub (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>-</p>"
+}
+function one (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>1</p>"
+}
+function two (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>2</p>"
+}
+function three (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>3</p>"
+}
+function add (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>+</p>"
+}
+function zero (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>0</p>"
+}
+function dot (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>.</p>"
+}
+function equal (){
+const input = document.querySelector(".input");
+input.innerHTML += "<p>=</p>"
+}
